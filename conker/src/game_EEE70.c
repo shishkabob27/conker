dc45f4ae3e330584001a2aca81641f60c86d4435
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-06-20 19:41:54 -0500

    Created game_EEE70.c with 1 C function

---
+++ conker/src/game_EEE70.c
@@ -0,0 +1,15 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_EEE70/func_150C19C0.s")
+
+//#pragma GLOBAL_ASM("asm/nonmatchings/game_EEE70/func_150C1A2C.s")
+s32 func_150C1A2C(s32 arg0, s32 arg1) {
+    return 7;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_EEE70/func_150C1A40.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_EEE70/func_150C1E34.s")
