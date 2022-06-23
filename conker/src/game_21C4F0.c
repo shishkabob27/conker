201385f25b9db7731c58b473c3358bc332cc28c8
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-06-22 21:31:34 -0500

    Create game_21C4F0

---
+++ conker/src/game_21C4F0.c
@@ -0,0 +1,22 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_21C4F0/func_151EF040.s")
+/*s32 func_151EF040(s32 arg0, s32 arg1) {
+    s32 temp_s0;
+
+    __osPiGetAccess();
+    temp_s0 = osPiRawReadIo(arg0, arg1);
+    __osPiRelAccess();
+    return temp_s0;
+}*/
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_21C4F0/func_151EF080.s")
+/*
+f32 func_151EF080(f32 arg0) {
+    return sqrtf(arg0);
+}
+*/
