b95c96fd85bf5ae698047e893c70849be85e3180
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2023-01-26 00:31:54 -0600

    more empty and lame func

---
+++ conker/src/game_1C2C60.c
@@ -0,0 +1,189 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_151957B0.s")
+
+void func_15195824(void) {
+
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519582C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15195868.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15195984.s")
+
+//#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15195A84.s")
+s16 func_15195A84(s16 arg0, s32 arg1, s32 arg2, s32 arg3) {
+    return arg0;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15195AA8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15195D00.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15195DD4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15195FB0.s")
+//void func_15195FB0(void *arg0, s32 arg4, s32 arg5, s32 arg6) {
+//    func_15195DD4(arg0->unk1C, arg4, arg5, arg6);
+//}
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15195FF0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15196318.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15196330.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_151963B4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15196438.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15196748.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15196B4C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15197148.s")
+
+f32 func_151979F8(s32 arg0) {
+    return D_800A8AA4;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15197A0C.s")
+
+f32 func_15197A68(s32 arg0) {
+    return D_800A8AAC;
+}
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15197A7C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15197AB4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15197BBC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15197C10.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15198054.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15198110.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15198570.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_151987CC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519897C.s")
+
+void func_15198C60(void) {
+    func_10010F30(0x1AA, 0x7FFF, 0x40, 0, 0);
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15198C90.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15198D40.s")
+
+void func_15198D7C(s32 arg0) {
+
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15198D88.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_151990AC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_151993B4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_151993E4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519944C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_151994B8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15199834.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519986C.s")
+
+void func_15199928(s32 arg0) {
+    func_15199980(arg0);
+    func_151478F4(arg0);
+}
+
+void func_15199954(s32 arg0) {
+    func_15199980(arg0);
+    func_15147928(arg0);
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15199980.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15199A10.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_15199C34.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519A9A4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519B4B8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519BE1C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519BEB8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519BF20.s")
+
+void func_1519BF8C(void) {
+    func_10010F30(0x1AA, 0x7FFF, 0x40, 0, 0);
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519BFBC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C06C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C09C.s")
+
+void func_1519C200(s32 arg0) {
+    func_1519C258(arg0);
+    func_151617C4(arg0);
+}
+
+void func_1519C22C(s32 arg0) {
+    func_1519C258(arg0);
+    func_151617E4(arg0);
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C258.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C26C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C4E4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C56C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C910.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C948.s")
+//f32 func_1519C948(void) {
+//    func_1519CDB0(0xD3E800000, 0x1C);
+//    return 0;
+//}
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C970.s")
+//s32 func_1519C970(void) {
+//    func_1519CDB0(0x3F000000, 0x1D);
+//    return 0;
+//}
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C998.s")
+//? func_1519C998(void) {
+//    func_1519CDB0(0x3E4CCCCD, 0x1D);
+//    return 0;
+//}
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519C9C4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519CD64.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_1C2C60/func_1519CDB0.s")
