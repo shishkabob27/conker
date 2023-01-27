a21ccbc456b19e30c5b20c9a2236945d40fa49cf
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2023-01-26 22:32:28 -0600

    whoops

--- conker/src/game_122760.c
+++ conker/src/game_122760.c
@@ -3,7 +3,7 @@
 #include "variables.h"
 
 
-//#pragma GLOBAL_ASM("asm/nonmatchings/game_122760/func_150F52B0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/game_122760/func_150F52B0.s")
 /*void func_150F52B0(void *arg0) {
     if (func_1509BE40(1, 0x401C, 6, 0x9000) != 0) {
         arg0->unk84 = arg0->unk84 | D_80000000;
