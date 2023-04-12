8e4431e3ea1ce0870703ee5df25e300bc9fa8de2
Shishkabob27 <>
2023-04-11 23:34:36 -0500

    2 whole functions

---
+++ conker/src/game_105760.c
@@ -0,0 +1,32 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+
+
+void func_150D82B0(s32 arg0) {
+
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_105760/func_150D82BC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_105760/func_150D83D8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_105760/func_150D8590.s")
+/*void *func_150D8590(struct120 *arg0, s32 arg1) {
+    arg0->unk0 = 0x42;
+    arg0->unk2 = 0;
+    return arg0 + 4; // HOW DO I MAKE IT COME OUT AS 4??
+}*/
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_105760/func_150D85AC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_105760/func_150D88AC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_105760/func_150D88E0.s")
+
+//#pragma GLOBAL_ASM("asm/nonmatchings/game_105760/func_150D8A20.s")
+s32 func_150D8A20(s32 arg0, s32 arg1) {
+    return 8;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_105760/func_150D8A34.s")
