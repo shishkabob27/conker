17edcc2da488b1e0b6e598d3135de0bdbcd0401c
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2023-04-27 18:22:56 -0500

    Added 4 functions to game_131A90

---
+++ conker/src/game_131A90.c
@@ -0,0 +1,25 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+
+
+void func_151045E0(s32 arg0, s32 arg1, s32 arg2) {
+
+}
+
+
+s32 func_151045F4(s32 arg0, s32 arg1) {
+    return 1;
+}
+
+
+void func_15104608(f32 arg0, f32 arg1, f32 arg2, f32 arg3) {
+  
+}
+
+
+s32 func_15104620(s32 arg0, s32 arg1) {
+    return 1;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_131A90/func_15104634.s")
