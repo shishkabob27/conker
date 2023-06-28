99525a3f660a2270175ce038d71d8320f7c0fb00
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2023-06-27 21:34:06 -0500

    also one function

---
+++ conker/src/game_C9EC0.c
@@ -0,0 +1,57 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CA10.s")
+/*
+s32 func_1509CA10(s32 arg0) {
+    return *(&D_80087430 + (arg0 * 20));
+}
+*/
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CA30.s")
+/*
+u16 func_1509CA30(s32 arg0) {
+    return *(&D_80087438 + (arg0 * 20));
+}
+*/
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CA50.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CA78.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CA98.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CB68.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CBD4.s")
+
+//#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CC94.s")
+void func_1509CC94() {
+    func_1509CCB4();
+}
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CCB4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CCF4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CDDC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CE64.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509CF28.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509D054.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509D08C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509D180.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509D780.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509D8FC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_C9EC0/func_1509DBBC.s")
